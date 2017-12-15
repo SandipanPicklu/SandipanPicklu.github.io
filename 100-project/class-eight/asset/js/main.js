b7c9a1200c693d4f7aa835eea5b16e9aefe833da
SandipanPicklu <sandipan.nstu@gmail.com>
2017-12-15 11:18:52 -0800

    class-eight

---
+++ 100-project/class-eight/asset/js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+$(".product-carousel").owlCarousel({
+items:4,
+margin:30,
+loop:true,
+autoplay:true,
+autoplayTimeout:1000,
+nav:true,
+navText:["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+
+});
+});
